f9c7b58cf15faafef1fc9497861e2bc18fed7180
Scott Grobe <gitgrobe@gmail.com>
2018-08-25 23:54:15 -0400

    Update insert_reddit_table_markdown.py

--- insert_reddit_table_markdown.py
+++ insert_reddit_table_markdown.py
@@ -2,9 +2,8 @@ import fileinput
 
 file_name = '/Users/Name/Desktop/freestyle_clearence_webscrape/clearence_product_list.txt'
 for line in fileinput.FileInput(file_name,inplace=1):
-	if ',' in line:
+	if '|' in line:
 		line = line.rstrip()
-		line = line.replace(',','|',)
 		line = line.replace('Price|Product','Price|Product'+'\n'+':-|:-')
 
 
